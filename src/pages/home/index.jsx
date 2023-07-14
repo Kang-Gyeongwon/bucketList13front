d2cbdd0877a52403dec5432243290aed2d919283
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-14 22:29:20 +0900

    feat: 기본 html 작성

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,28 @@
 const HomePage = () => {
-  return <>HomePage</>;
+  return (
+    <>
+      <div>Bucket List</div>
+      <div>
+        <div
+          style={{
+            backgroundImage: "url(/assets/images/main-image.jpg)",
+            height: "500px",
+            width: "500px",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <span>Welcome to your bucket list</span>
+        <div>
+          Life is an exciting journey filled with dreams and possibilities.
+          Every adventure begins with a single step. Start your bucket list
+          journey today and make your dreams come true one by one. Because your
+          dreams are not just dreams, they are a roadmap to your future. So why
+          wait? Start your journey now!
+        </div>
+        <button>Go To My Bucket List</button>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
