0dad67a9a82b6f5d99f427dae533bf238321f9e4
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-14 21:57:52 +0900

    design: 전역 스타일링

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home";
 import BucketList from "./pages/bucketlist";
+import { GlobalStyle } from "./style/GlobalStyle";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/bucketlist" element={<BucketList />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/bucketlist" element={<BucketList />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
