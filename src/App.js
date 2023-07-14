b86855c6eb90bf7bcfd4e6f6aab7715da658dc92
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-14 21:01:10 +0900

    feat: 기본적인 페이지 url 작업

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 const App = () => {
-  return <duv>App</duv>
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<div>HomePage</div>} />
+    <Route path="/bucketlist" element={<div>BucketListPage</div>} />
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App
