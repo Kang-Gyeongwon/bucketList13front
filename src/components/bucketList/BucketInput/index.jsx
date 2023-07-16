4ee41ebc308f68fa4fe8ecbdd0f2e52d39fe7e58
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-17 00:45:13 +0900

    docs: input 에 글자수 제한 문구 넣기

--- src/components/bucketList/BucketInput/index.jsx
+++ src/components/bucketList/BucketInput/index.jsx
@@ -3,7 +3,10 @@ import { AddBtn, FormInput, InputBox } from "./styled";
 const BucketInput = () => {
   return (
     <InputBox>
-      <FormInput type="text" placeholder="What's on your adventure list?" />
+      <FormInput
+        type="text"
+        placeholder="What's on your adventure list? You can enter up to 140 characters."
+      />
       <AddBtn type="submit" value="Create" />
     </InputBox>
   );
