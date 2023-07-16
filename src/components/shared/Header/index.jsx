1a2dfc320fdce2ab23f47f51d3acea4d4de17605
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-17 01:22:57 +0900

    feat: home 버튼 완료

--- src/components/shared/Header/index.jsx
+++ src/components/shared/Header/index.jsx
@@ -1,13 +1,15 @@
 import { HeaderContainer, HeaderTitle } from "./styled";
 import ListIcon from "../ListIcon";
 
-const Header = () => {
+const Header = (props) => {
+  const { children } = props;
   return (
     <HeaderContainer>
       <HeaderTitle>
         <ListIcon />
         <>Bucket List</>
       </HeaderTitle>
+      {children}
     </HeaderContainer>
   );
 };
