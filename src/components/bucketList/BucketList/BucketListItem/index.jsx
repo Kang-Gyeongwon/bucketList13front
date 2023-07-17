b7ab4025724f877be6520d5dc19ab13fcb582937
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-18 01:04:52 +0900

    feat: 완료버튼 기능 구현하기

--- src/components/bucketList/BucketList/BucketListItem/index.jsx
+++ src/components/bucketList/BucketList/BucketListItem/index.jsx
@@ -9,11 +9,35 @@ import {
   checkBoxStyle,
 } from "./styled";
 import Checkbox from "../../../shared/CheckBox";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
+import { completeBucketItem } from "../../../../api/bucketItems";
 
 const BucketListItem = (props) => {
   const { item } = props;
+
+  const [isCompleted, setIsCompleted] = useState(item.finish_check);
+
+  const queryClient = useQueryClient();
+
+  const updateCompletedMutation = useMutation(
+    (newStatus) =>
+      completeBucketItem(item.id, { ...item, finish_check: newStatus }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("bucketListItems");
+      },
+    }
+  );
+
   const handleClose = () => {
     console.log("You closed the card.");
+  };
+
+  const handleCheck = () => {
+    const newStatus = !isCompleted;
+    setIsCompleted(newStatus);
+    updateCompletedMutation.mutate(newStatus);
   };
 
   return (
@@ -24,7 +48,11 @@ const BucketListItem = (props) => {
       <ContentContainer>
         <ImageBox></ImageBox>
         <ContentBox>
-          <Checkbox style={checkBoxStyle} />
+          <Checkbox
+            style={checkBoxStyle}
+            checked={isCompleted}
+            onClick={handleCheck}
+          />
           <Content>{item.content}</Content>
         </ContentBox>
       </ContentContainer>
