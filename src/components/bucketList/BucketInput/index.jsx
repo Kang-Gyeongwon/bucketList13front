d2c0c75df2f4b2c02605d8a29bd7bfce1e42abdb
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-17 21:37:13 +0900

    feat: Create누르면 데이터 추가 하기

--- src/components/bucketList/BucketInput/index.jsx
+++ src/components/bucketList/BucketInput/index.jsx
@@ -1,13 +1,54 @@
+import { useState } from "react";
 import { AddBtn, FormInput, InputBox } from "./styled";
+import { QueryClient, useMutation } from "react-query";
+import { addBucketItem } from "../../../api/bucketItems";
 
 const BucketInput = () => {
+  const [formValue, setFormValue] = useState({
+    content: "",
+  });
+
+  const queryClient = new QueryClient();
+
+  const mutation = useMutation(addBucketItem, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("bucketListItems");
+    },
+  });
+
+  const handkeInputChages = (e) => {
+    setFormValue({
+      ...formValue,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (formValue.content.length > 140) {
+      window.alert("Please enter up to 140 characters.");
+      return;
+    }
+    const newBucket = {
+      ...formValue,
+      finish_check: false,
+    };
+    mutation.mutate(newBucket);
+
+    setFormValue({
+      content: "",
+    });
+  };
+
   return (
     <InputBox>
       <FormInput
         type="text"
+        name="content"
+        value={formValue.content}
+        onChange={handkeInputChages}
         placeholder="What's on your adventure list? You can enter up to 140 characters."
       />
-      <AddBtn type="submit" value="Create" />
+      <AddBtn type="submit" value="Create" onClick={handleSubmit} />
     </InputBox>
   );
 };
